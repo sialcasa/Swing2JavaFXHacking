d7eeae7fa216d1ffa4ea54d66f457e405422a05a
Alexander Casall <alexander.casall@saxsys.de>
2014-10-28 15:05:32 +0100

    Working Splitpane

--- src/main/java/com/github/twitterswingsample/view/frames/MainFrame.java
+++ src/main/java/com/github/twitterswingsample/view/frames/MainFrame.java
@@ -8,6 +8,13 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.embed.swing.SwingNode;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
 import javax.swing.JCheckBoxMenuItem;
@@ -16,7 +23,6 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JSplitPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.metal.MetalLookAndFeel;
@@ -29,6 +35,7 @@ import com.github.twitterswingsample.view.listener.LoginDialogCreator;
 import com.github.twitterswingsample.view.listener.ProgramCloser;
 import com.github.twitterswingsample.view.listener.RemoveAccountListener;
 import com.github.twitterswingsample.view.listener.SwitchAccountListener;
+import com.github.twitterswingsample.view.migration.console.ConsolePanelFX;
 import com.github.twitterswingsample.view.panels.ClientUserPanel;
 import com.github.twitterswingsample.view.panels.ConsolePanel;
 import com.github.twitterswingsample.view.panels.ShortInfoPanel;
@@ -38,135 +45,138 @@ import com.github.twitterswingsample.view.panels.ShortInfoPanel;
  * 
  * @author sourcefranke
  */
-public class MainFrame extends JFrame{
-	
-	private JPanel cardPanel;
-	private CardLayout layout;
-	private ButtonGroup group;
-	private JMenu switchUser,
-				removeUser;
-	
-	public MainFrame() {
-		Properties properties = new Properties();
-		BufferedInputStream stream;
-		String title = "Twitter4J Swing Sample ";
-		try {
-			stream = new BufferedInputStream(getClass().getResourceAsStream("/version.properties"));
-			properties.load(stream);
-			stream.close();
-			title += properties.getProperty("version");
-		} catch (IOException e) {
-			title += "(unknown version)";
-		}
-		setTitle(title);
-		
-		setBounds(50, 0, 600, 700);
-		setExtendedState(MAXIMIZED_VERT);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(new BorderLayout(2, 2));
-		try {
-			MetalLookAndFeel.setCurrentTheme(new TwitterTheme());
-			UIManager.setLookAndFeel(new MetalLookAndFeel());
-		} catch (UnsupportedLookAndFeelException e1) {
-			ConsolePanel.getInstance().printMessage(new String[]{
-				"Look-And-Feel could not be set",
-				e1.getLocalizedMessage()
-			});
-		}
-		
-		JMenuBar jmb = new JMenuBar();
-		
-		JMenu file = new JMenu("File");
-		JMenuItem exit = new JMenuItem("Close");
-		exit.addActionListener(new ProgramCloser());
-		file.add(exit);
-		jmb.add(file);
-		
-		JMenu user = new JMenu("Account");
-		switchUser = new JMenu("Switch account");
-		removeUser = new JMenu("Remove account");
-		JMenuItem addUser = new JMenuItem("Add account ...");
-		addUser.addActionListener(new LoginDialogCreator(this));
-		user.add(addUser);
-		user.add(switchUser);
-		user.add(removeUser);
-		jmb.add(user);
-		
-		setJMenuBar(jmb);
-		
-		try {
-			setIconImage(ImageIO.read(getClass().getResource("images/icon.png")));
-		} catch (IOException e) {}
+public class MainFrame extends JFrame {
 
-		JSplitPane vertical = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		vertical.setBottomComponent(ConsolePanel.getInstance());
-		vertical.setResizeWeight(1.);
-		vertical.setOneTouchExpandable(true);
-		
-		try {
-			Credentials creds = new Credentials();
-			layout = new CardLayout();
-			cardPanel = new JPanel(layout);
-			group = new ButtonGroup();
-			
-			List<Integer> ids = creds.getIDs();
-			List<String> names = creds.getNames();
-			List<Twitter> twitters = creds.getTwitters();
-			for (int i = 0; i < twitters.size(); i++) {
-				addItemsAndPanels(creds, names.get(i), ids.get(i), twitters.get(i));
-			}
-			
-			if(group.getButtonCount() > 0) {
-				group.getElements().nextElement().setSelected(true);
-			}
-			vertical.setTopComponent(cardPanel);
-		} catch (SQLException e) {
-			ConsolePanel.getInstance().printMessage(new String[]{
-					"SQL Exception thrown, there is an incorrect SQL statement",
-					"Please report that bug!",
-					e.getLocalizedMessage()
-			});
-		} catch (ClassNotFoundException e) {
-			ConsolePanel.getInstance().printMessage(new String[]{
-					"SQLite driver couldn't be found",
-					"Please report that bug!",
-					e.getLocalizedMessage()
-			});
-		} catch (Exception e) {
-			ConsolePanel.getInstance().printMessage(new String[]{
-					"Unspecified Error",
-					"Please report that bug!",
-					e.getLocalizedMessage()
-			});
-		}
+    private JPanel cardPanel;
+    private CardLayout layout;
+    private ButtonGroup group;
+    private final JMenu switchUser, removeUser;
 
-		add(vertical, BorderLayout.CENTER);
-		add(ShortInfoPanel.getInstance(), BorderLayout.SOUTH);
-	}
-	
-	public void addUser(Credentials creds, int id) throws ClassNotFoundException, SQLException {
-		cardPanel.setVisible(false);
-		addItemsAndPanels(creds, creds.getName(id), id, creds.getTwitter(id));
-		cardPanel.setVisible(true);
-	}
-	
-	private void addItemsAndPanels(Credentials creds, String name, int id, Twitter twitter) {
-		JCheckBoxMenuItem switchItem = new JCheckBoxMenuItem(name);
-		switchItem.addActionListener(new SwitchAccountListener(layout, cardPanel, name));
-		group.add(switchItem);
-		switchUser.add(switchItem);
-		cardPanel.add(name, new ClientUserPanel(twitter));
-		
-		if(group.getButtonCount() == 1) {
-			switchItem.setSelected(true);
-		}
-		
-		JMenuItem removeItem = new JMenuItem(name);
-		removeItem.addActionListener(new RemoveAccountListener(creds, id));
-		removeUser.add(removeItem);
-	}
-	
-	public static void main(String[] args){
-		new MainFrame().setVisible(true);
-	}
+    public MainFrame() {
+        Properties properties = new Properties();
+        BufferedInputStream stream;
+        String title = "Twitter4J Swing Sample ";
+        try {
+            stream = new BufferedInputStream(getClass().getResourceAsStream("/version.properties"));
+            properties.load(stream);
+            stream.close();
+            title += properties.getProperty("version");
+        } catch (IOException e) {
+            title += "(unknown version)";
+        }
+        setTitle(title);
+
+        setBounds(50, 0, 600, 700);
+        setExtendedState(MAXIMIZED_VERT);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(2, 2));
+        try {
+            MetalLookAndFeel.setCurrentTheme(new TwitterTheme());
+            UIManager.setLookAndFeel(new MetalLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e1) {
+            ConsolePanel.getInstance().printMessage(
+                    new String[] { "Look-And-Feel could not be set", e1.getLocalizedMessage() });
+        }
+
+        JMenuBar jmb = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        JMenuItem exit = new JMenuItem("Close");
+        exit.addActionListener(new ProgramCloser());
+        file.add(exit);
+        jmb.add(file);
+
+        JMenu user = new JMenu("Account");
+        switchUser = new JMenu("Switch account");
+        removeUser = new JMenu("Remove account");
+        JMenuItem addUser = new JMenuItem("Add account ...");
+        addUser.addActionListener(new LoginDialogCreator(this));
+        user.add(addUser);
+        user.add(switchUser);
+        user.add(removeUser);
+        jmb.add(user);
+
+        setJMenuBar(jmb);
+
+        try {
+            setIconImage(ImageIO.read(getClass().getResource("images/icon.png")));
+        } catch (IOException e) {
+        }
+
+        // JSplitPane vertical = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+
+        SplitPane vertical = new SplitPane();
+        vertical.setOrientation(Orientation.VERTICAL);
+
+        vertical.getItems().add(ConsolePanelFX.getInstance());
+
+        try {
+            Credentials creds = new Credentials();
+            layout = new CardLayout();
+            cardPanel = new JPanel(layout);
+            group = new ButtonGroup();
+
+            List<Integer> ids = creds.getIDs();
+            List<String> names = creds.getNames();
+            List<Twitter> twitters = creds.getTwitters();
+            for (int i = 0; i < twitters.size(); i++) {
+                addItemsAndPanels(creds, names.get(i), ids.get(i), twitters.get(i));
+            }
+
+            if (group.getButtonCount() > 0) {
+                group.getElements().nextElement().setSelected(true);
+            }
+
+            SwingNode cardPanelWrapper = new SwingNode();
+            cardPanelWrapper.setContent(cardPanel);
+            Platform.runLater(() -> vertical.getItems().add(0, cardPanelWrapper));
+
+        } catch (SQLException e) {
+            ConsolePanel.getInstance().printMessage(
+                    new String[] { "SQL Exception thrown, there is an incorrect SQL statement",
+                            "Please report that bug!", e.getLocalizedMessage() });
+        } catch (ClassNotFoundException e) {
+            ConsolePanel.getInstance().printMessage(
+                    new String[] { "SQLite driver couldn't be found", "Please report that bug!",
+                            e.getLocalizedMessage() });
+        } catch (Exception e) {
+            ConsolePanel.getInstance().printMessage(
+                    new String[] { "Unspecified Error", "Please report that bug!", e.getLocalizedMessage() });
+        }
+
+        Platform.runLater(() -> {
+            JFXPanel verticalWrapper = new JFXPanel();
+            verticalWrapper.setScene(new Scene(vertical));
+            add(verticalWrapper, BorderLayout.CENTER);
+        });
+
+        add(ShortInfoPanel.getInstance(), BorderLayout.SOUTH);
+    }
+
+    public void addUser(Credentials creds, int id) throws ClassNotFoundException, SQLException {
+        cardPanel.setVisible(false);
+        addItemsAndPanels(creds, creds.getName(id), id, creds.getTwitter(id));
+        cardPanel.setVisible(true);
+    }
+
+    private void addItemsAndPanels(Credentials creds, String name, int id, Twitter twitter) {
+        JCheckBoxMenuItem switchItem = new JCheckBoxMenuItem(name);
+        switchItem.addActionListener(new SwitchAccountListener(layout, cardPanel, name));
+        group.add(switchItem);
+        switchUser.add(switchItem);
+        cardPanel.add(name, new ClientUserPanel(twitter));
+
+        if (group.getButtonCount() == 1) {
+            switchItem.setSelected(true);
+        }
+
+        JMenuItem removeItem = new JMenuItem(name);
+        removeItem.addActionListener(new RemoveAccountListener(creds, id));
+        removeUser.add(removeItem);
+    }
+
+    public static void main(String[] args) {
+        new JFXPanel();
+        new MainFrame().setVisible(true);
+    }
 }
